38dbf828e38c8762631b3f864df88960c24a940c
Prudhvi <146826315+prudhvi-battu@users.noreply.github.com>
2025-02-15 22:14:56 +0530

    Add files via upload

---
+++ scripts/insertion_sort.js
@@ -0,0 +1,51 @@
+function Insertion()
+{
+    //Setting Time complexities
+    document.getElementById("Time_Worst").innerHTML="O(N<sup>2</sup>)";
+    document.getElementById("Time_Average").innerHTML="O(N<sup>2</sup>)";
+    document.getElementById("Time_Best").innerText="O(N)";
+
+    // DESCRIPTION
+    var descriptionElement = document.getElementById("description");
+    descriptionElement.innerHTML = '<a href="insertion_sort.html" target="_self">FUNCTIONING</a>';
+
+    c_delay=0;
+
+    for(var j=0;j<array_size;j++)
+    {
+        div_update(divs[j],div_sizes[j],"yellow");//Color update
+
+        var key= div_sizes[j];
+        var i=j-1;
+        while(i>=0 && div_sizes[i]>key)
+        {
+            div_update(divs[i],div_sizes[i],"red");//Color update
+            div_update(divs[i+1],div_sizes[i+1],"red");//Color update
+
+            div_sizes[i+1]=div_sizes[i];
+
+            div_update(divs[i],div_sizes[i],"red");//Height update
+            div_update(divs[i+1],div_sizes[i+1],"red");//Height update
+    
+            div_update(divs[i],div_sizes[i],"blue");//Color update
+            if(i==(j-1))
+            {
+                div_update(divs[i+1],div_sizes[i+1],"yellow");//Color update
+            }
+            else
+            {
+                div_update(divs[i+1],div_sizes[i+1],"blue");//Color update
+            }
+            i-=1;
+        }
+        div_sizes[i+1]=key;
+
+        for(var t=0;t<j;t++)
+        {
+            div_update(divs[t],div_sizes[t],"green");//Color update
+        }
+    }
+    div_update(divs[j-1],div_sizes[j-1],"green");//Color update
+
+    enable_buttons();
+}
---
+++ scripts/selection_sort.js
@@ -0,0 +1,54 @@
+function Selection_sort()
+{
+    //Setting Time complexities
+    document.getElementById("Time_Worst").innerHTML="O(N<sup>2</sup>)";
+    document.getElementById("Time_Average").innerHTML="O(N<sup>2</sup>)";
+    document.getElementById("Time_Best").innerHTML="O(N<sup>2</sup>)";
+
+    //Setting Description
+    var descriptionElement = document.getElementById("description");
+    descriptionElement.innerHTML = '<a href="selection_sort.html" target="_self">FUNCTIONING</a>';
+
+    c_delay=0;
+
+    for(var i=0;i<array_size-1;i++)
+    {
+        div_update(divs[i],div_sizes[i],"red");//Color update
+
+        index_min=i;
+
+        for(var j=i+1;j<array_size;j++)
+        {
+            div_update(divs[j],div_sizes[j],"yellow");//Color update
+
+            if(div_sizes[j]<div_sizes[index_min])
+            {
+                if(index_min!=i)
+                {
+                    div_update(divs[index_min],div_sizes[index_min],"blue");//Color update
+                }
+                index_min=j;
+                div_update(divs[index_min],div_sizes[index_min],"red");//Color update
+            }
+            else
+            {
+                div_update(divs[j],div_sizes[j],"blue");//Color update
+            }
+        }
+        
+        if(index_min!=i)
+        {
+            var temp=div_sizes[index_min];
+            div_sizes[index_min]=div_sizes[i];
+            div_sizes[i]=temp;
+
+            div_update(divs[index_min],div_sizes[index_min],"red");//Height update
+            div_update(divs[i],div_sizes[i],"red");//Height update
+            div_update(divs[index_min],div_sizes[index_min],"blue");//Color update
+        }
+        div_update(divs[i],div_sizes[i],"green");//Color update
+    }
+    div_update(divs[i],div_sizes[i],"green");//Color update
+
+    enable_buttons();
+}
